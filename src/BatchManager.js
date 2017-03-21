d7fb67efa2c28cc754fe3b2da8cb85396e05624c
Michiel Westerbeek <happylinks@gmail.com>
2017-03-21 14:21:45 +0100

    Update BatchManager.js

--- src/BatchManager.js
+++ src/BatchManager.js
@@ -9,46 +9,46 @@ class BatchManager {
     }
 
     setupSubscriptions() {
-		const query = gql`
-			query assets {
-				viewer {
-					allAssets {
-						edges {
-							node {
-								id
-							}
-						}
-					}
-				}
-			}
-		`;
-		const subscription = gql`
-			subscription Asset {
-				Asset {
-					mutation
-					node {
-						id
-					}
-				}
-			}
-		`;
-		const queryHandle = apolloClient.watchQuery({
-			query
-		});
-		queryHandle.subscribe({
-      		next(queryResult) {
-				console.log('hiero');
-				console.log(queryResult);
-      		},
-    	});
-		queryHandle.subscribeToMore({
-      		document: subscription,
-      		updateQuery: (prev, { subscriptionData }) => {
-				console.log('hiero2');
-				console.log(subscriptionData);
-				return;
-			}
-    	});
+        const query = gql`
+            query assets {
+                viewer {
+                    allAssets {
+                        edges {
+                            node {
+                                id
+                            }
+                        }
+                    }
+                }
+            }
+        `;
+        const subscription = gql`
+            subscription Asset {
+                Asset {
+                    mutation
+                    node {
+                        id
+                    }
+                }
+            }
+        `;
+        const queryHandle = apolloClient.watchQuery({
+            query
+        });
+        queryHandle.subscribe({
+            next(queryResult) {
+                console.log('hiero');
+                console.log(queryResult);
+            },
+        });
+        queryHandle.subscribeToMore({
+            document: subscription,
+            updateQuery: (prev, { subscriptionData }) => {
+                console.log('hiero2');
+                console.log(subscriptionData);
+                return;
+            }
+        });
 	}
 
     add(batch) {
@@ -61,7 +61,6 @@ class BatchManager {
             batch.start();
         });
     }
-
 }
 
 export default BatchManager;
